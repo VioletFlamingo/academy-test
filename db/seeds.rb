aeb638bb174cf9514af1fb1a9cfaa39484393e8e
Paulina Winkowska <paulina.winkowska@gmail.com>
2015-02-27 21:12:27 +0100

    Task 6.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,15 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+city_list = [
+    ["Paris", "Pr"],
+    ["London", "Ld"],
+    ["Warsaw", "Wr"],
+    ["Tokyo", "Tk"],
+    ["New York", "Ny"],
+    ["Madrid", "Md"]
+]
+
+city_list.each do |name, short_name|
+  City.create(name: name, short_name: short_name)
+end
